a87c7393f92a569eb4e0ee01310e572bf0122037
Vaidas <shibalba1@gmail.com>
2019-11-16 15:27:57 +0100

    mv Tests to tests

---
+++ tests/LogicItLab/Salesforce/MapperBundle/Mock/DescribeAccountResult.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\LogicItLab\Salesforce\MapperBundle\Mock;
+
+use Phpforce\SoapClient\Result\DescribeSObjectResult;
+use Phpforce\SoapClient\Result\DescribeSObjectResult\Field;
+
+class DescribeAccountResult extends DescribeSObjectResult
+{
+    public function __construct()
+    {
+        $this->fields[] = new FieldId();
+        $this->fields[] = new FieldName();
+    }
+}
+
+class FieldName extends Field
+{
+    protected $name = 'Name';
+    protected $createable = true;
+    protected $updateable = true;
+}
+
+class FieldId extends Field
+{
+    protected $name = 'Id';
+    protected $createable = true;
+    protected $updateable = true;
+}
