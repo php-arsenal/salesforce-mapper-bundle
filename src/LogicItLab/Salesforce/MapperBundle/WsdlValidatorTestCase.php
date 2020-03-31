55d86fe21274f1338fd335b342eace006f22fd8e
Luca Cermenati <luca.cermenati@comsave.com>
2020-03-31 11:04:49 +0200

    Avoided Kernel use for test case

--- src/LogicItLab/Salesforce/MapperBundle/WsdlValidatorTestCase.php
+++ src/LogicItLab/Salesforce/MapperBundle/WsdlValidatorTestCase.php
@@ -2,15 +2,11 @@
 
 namespace LogicItLab\Salesforce\MapperBundle;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
+use LogicItLab\Salesforce\MapperBundle\Annotation\AnnotationReader;
+use PHPUnit\Framework\TestCase;
 
-abstract class WsdlValidatorTestCase extends KernelTestCase
+abstract class WsdlValidatorTestCase extends TestCase
 {
-    /** @var KernelInterface */
-    protected $bootedKernel;
-
     public abstract function modelAndWsdlDataProvider(): array;
 
     /**
@@ -20,38 +16,12 @@ abstract class WsdlValidatorTestCase extends KernelTestCase
      */
     public function testWsdlIsValid($modelsDir, $wsdlPath)
     {
-        $this->markTestIncomplete();
-
-        $validator = $this->buildValidator();
-        $this->assertEmpty($validator->validate($modelsDir, $wsdlPath));
-    }
-
-    public function getParameter(string $parameterName)
-    {
-        return $this->getContainer()->getParameter($parameterName);
+        $this->assertEmpty($this->buildValidator()->validate($modelsDir, $wsdlPath));
     }
 
     private function buildValidator(): WsdlValidator
     {
-        $this->getService(WsdlValidator::class);
-    }
-
-    private function getService(string $className)
-    {
-        return $this->getContainer()->get($className);
-    }
-
-    private function getContainer(): ContainerInterface
-    {
-        return $this->getKernel()->getContainer();
-    }
-
-    private function getKernel(): KernelInterface
-    {
-        if (!$this->bootedKernel) {
-            $this->bootedKernel = $this->bootKernel();
-        }
-
-        return $this->bootedKernel;
+        $annotationReader = new AnnotationReader(new \Doctrine\Common\Annotations\AnnotationReader());
+        return new WsdlValidator($annotationReader);
     }
 }
